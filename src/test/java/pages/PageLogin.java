6c98b28a0aee2c3ec39f85d32b9c5069fc5826d5
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-07-12 17:12:21 +0530

    PageLogin.java updated

--- src/test/java/pages/PageLogin.java
+++ src/test/java/pages/PageLogin.java
@@ -1,9 +1,32 @@
 package pages;
 
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PageLogin extends BasePage{
     public PageLogin(WebDriver driver) {
         super(driver);
+    }
+
+    @AndroidFindBy(accessibility = "input-email")
+    private WebElement tf_userName;
+
+    @AndroidFindBy(accessibility = "input-password")
+    private WebElement tf_password;
+
+    @AndroidFindBy(accessibility = "button-LOGIN\n")
+    private WebElement btn_login;
+
+    public void fillUserName(String userName){
+        tf_userName.sendKeys(userName);
+    }
+
+    public void fillPassword(String password){
+        tf_password.sendKeys(password);
+    }
+
+    public void clickOnLoginButton(){
+        btn_login.click();
     }
 }
